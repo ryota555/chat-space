b711cd1957e93909302fb3c00447cfdae9a79fec
井原瞭太 <>
2018-10-07 00:02:37 +0900

    Revert "missing"
    This reverts commit b504e60b659b32f9de6e811072a39c1dec4066c4.

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
+  resources :messages, only: [:index]
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
-  resources :messages, only: [:index, :create]
 end
