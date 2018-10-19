ea31203016c6783a122e9b51697569d888cf5062
井原瞭太 <>
2018-10-20 01:32:49 +0900

    ajax

--- chat-space/app/assets/javascripts/user.js
+++ chat-space/app/assets/javascripts/user.js
@@ -1,6 +1,12 @@
 $(function() {
   $("#user-search-field").on("keyup", fuction() {
     var input = $(this).val();
-    console.log(input);
+
+    $.ajax({
+      type: 'GET',
+      url: '/products/search'
+      date: { keyword: input },
+      datetype: 'json'
+    })
   });
 });
