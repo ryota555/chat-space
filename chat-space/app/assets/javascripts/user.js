b724c9767b7e1f35aedc7d95761b95b299ad24d3
井原瞭太 <>
2018-10-20 15:00:32 +0900

    create html

--- chat-space/app/assets/javascripts/user.js
+++ chat-space/app/assets/javascripts/user.js
@@ -1,4 +1,18 @@
 $(function() {
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">
+                    ${user.name}
+                  </p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
+                  </a>
+                </div>`
+    $("#user-search-result").append(html);
+  }
+
+  }
+
   $("#user-search-field").on("keyup", fuction() {
     var input = $(this).val();
 
@@ -9,8 +23,16 @@ $(function() {
       datetype: 'json'
     })
 
-    .done(function(products) {
-      $("")
-    }
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !==0) {
+        users.forEach(function(user){
+        appendUser(user);
+        });
+      }
+      else{
+        appendNoUser("一致するユーザーはいません");
+      }
+    })
   });
 });
