80c3fd6def87ebd3be9040cc0162decc52d5dbfe
井原瞭太 <>
2018-10-20 15:21:08 +0900

    add member

--- chat-space/app/assets/javascripts/user.js
+++ chat-space/app/assets/javascripts/user.js
@@ -11,6 +11,16 @@ $(function() {
     $("#user-search-result").append(html);
   }
 
+  function addGroup(user_id,user_name){
+    var html =
+      `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+        <input name='group[user_ids][]' type='hidden' value="${user_id}">
+        <p class='chat-group-user__name'>
+          ${user_name}
+        </p>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+      </div>`
+    $("#chat-group-users").append(html);
   }
 
   $("#user-search-field").on("keyup", fuction() {
@@ -18,7 +28,7 @@ $(function() {
 
     $.ajax({
       type: 'GET',
-      url: '/users/index'
+      url: '/users/index',
       date: { keyword: input },
       datetype: 'json'
     })
@@ -31,11 +41,19 @@ $(function() {
         });
       }
       else{
-        appendNoUser("一致するユーザーはいません");
+        appendNoUser("一致するユーザーはいません")
       }
     })
     .fail(function(user) {
       alert('検索に失敗しました');
-    })
+    });
   });
+
+  $("#user-search-result").on("click", ".user-search-add", function(){
+    var user_name = $(this).data('userName');
+    var user_id = $(this).data('userId');
+    addGroup(user_id,user_name);
+    $(this).parent().add();
+  });
+
 });
