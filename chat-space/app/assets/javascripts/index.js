97ec08c80a80b5c2a9d5f823461148149d7b05e2
井原瞭太 <>
2018-10-22 02:55:50 +0900

    edit index.js

--- chat-space/app/assets/javascripts/index.js
+++ chat-space/app/assets/javascripts/index.js
@@ -1,10 +1,26 @@
 $(fubction(){
+  function buildHTML(message) {
+    var html = `<div class="chat">
+                  <div class="chat__name">
+                   ${messagese.user.name}
+                  </div>
+                  <div class="chat__message">
+                    ${message.content}
+                  </div>
+                </div>`
+    return html;
+  }
+
   $(function(){
     setInterval(update, 5000);
   });
 
   function update(){
-    var message_id = $('.messages:last').date('id');
+    if($('.messages')[0]){
+      var message_id = $('.messages:last').date('id');
+    }else{
+      var message_id = 0
+    }
     $ajax({
       url :location.href,
       type: 'GET',
@@ -13,5 +29,10 @@ $(fubction(){
       },
       dateType: 'json'
     })
+    .always(function(data){
+      $.each(date, function(i, data){
+        buildHTML(data);
+      });
+    });
   }
 });
